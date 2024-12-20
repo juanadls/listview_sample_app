70b95ccc67ecbc45daa78bfeca11e4dfc3a5057e
juanadls <eleo.dev09@gmail.com>
2024-12-20 13:00:52 -0300

    listview added

--- app/src/main/java/com/example/listview_sample_app/MainActivity.java
+++ app/src/main/java/com/example/listview_sample_app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listview_sample_app;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //Listview is the adapter view
+        listView = findViewById(R.id.listview);
+
+        // data source
+        String[] countries = {"USA", "Germany", "Saudi Arabia", "France"};
+
+        //Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1,
+                countries
+        );
+        listView.setAdapter(adapter);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
